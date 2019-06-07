695412afc03756e83c5d22fe6b54ebdcdd9c650f
Vien Hong <vienhong88@gmail.com>
2019-06-06 23:02:31 -0400

    brainstorm

---
+++ logic.js
@@ -0,0 +1,6 @@
+//Input the numbers of persons per table.
+//Input the amounts of services.
+//Calculate the tips.
+//Calculate the total.
+//Calculate the amount each after SLIP.
+//Return the amount each.
